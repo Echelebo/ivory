64a70af142db132a7cf347ef9162c819c859db7c
Echelebo <echelebochibuike@gmail.com>
2025-02-13 22:53:40 +0100

    First

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -546,6 +546,11 @@ $page_title = "Home";
         <!-- department area end -->
 
 
+
+    @php
+    $section_testimonials = \App\Models\Section::section('testimonials');
+@endphp
+@if(count($testimonials) > 0 && isset($section_testimonials))
         <!-- testimonial area -->
         <div class="testimonial-area ts-bg pt-80 pb-80">
             <div class="container">
@@ -560,6 +565,7 @@ $page_title = "Home";
                 </div>
                 <div class="testimonial-slider owl-carousel owl-theme">
                     <div class="testimonial-item">
+                        @foreach($testimonials as $testimonial)
                         <div class="testimonial-rate">
                             <i class="fas fa-star"></i>
                             <i class="fas fa-star"></i>
@@ -569,109 +575,24 @@ $page_title = "Home";
                         </div>
                         <div class="testimonial-quote">
                             <p>
-                            Joining Ivorygate education center was one of the best decisions I’ve ever made. The personalized attention I receive from my tutors has helped me improve my grades in math significantly.
+                                {!! $testimonial->description !!}
                             </p>
                         </div>
                         <div class="testimonial-content">
                             <div class="testimonial-author-info">
-                                <h4>Anthony</h4>
-                                <p>Student</p>
+                                <h4>{{ $testimonial->title }}</h4>
+                                <p>{{ $testimonial->designation }}</p>
                             </div>
                         </div>
                         <span class="testimonial-quote-icon"><i class="far fa-quote-right"></i></span>
+                        @endforeach
                     </div>
-                    <div class="testimonial-item">
-                        <div class="testimonial-rate">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                        </div>
-                        <div class="testimonial-quote">
-                            <p>
-                            The support from my teachers made all the difference during my college application process. They guided me through every step, and I’m proud to say I got accepted into my dream university thanks to their help!
-                            </p>
-                        </div>
-                        <div class="testimonial-content">
 
-                            <div class="testimonial-author-info">
-                                <h4>Richard</h4>
-                                <p>Student</p>
-                            </div>
-                        </div>
-                        <span class="testimonial-quote-icon"><i class="far fa-quote-right"></i></span>
-                    </div>
-                    <div class="testimonial-item">
-                        <div class="testimonial-rate">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                        </div>
-                        <div class="testimonial-quote">
-                            <p>
-                            I love the after-school programs! I’ve made so many friends and learned new skills in art and robotics. It makes school so much more fun and engaging!
-                            </p>
-                        </div>
-                        <div class="testimonial-content">
-
-                            <div class="testimonial-author-info">
-                                <h4>Ogechi</h4>
-                                <p>Student</p>
-                            </div>
-                        </div>
-                        <span class="testimonial-quote-icon"><i class="far fa-quote-right"></i></span>
-                    </div>
-                    <div class="testimonial-item">
-                        <div class="testimonial-rate">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                        </div>
-                        <div class="testimonial-quote">
-                            <p>
-                            I cannot express how grateful I am for the support my son received at Ivorygate education center. The staff is incredibly caring and dedicated, and I can see the growth in his confidence and academic performance.
-                            </p>
-                        </div>
-                        <div class="testimonial-content">
-
-                            <div class="testimonial-author-info">
-                                <h4>Linda T.</h4>
-                                <p>Parent</p>
-                            </div>
-                        </div>
-                        <span class="testimonial-quote-icon"><i class="far fa-quote-right"></i></span>
-                    </div>
-                    <div class="testimonial-item">
-                        <div class="testimonial-rate">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                        </div>
-                        <div class="testimonial-quote">
-                            <p>
-                            As a parent, I appreciate how Ivorygate education center emphasizes not just academic success but also personal development.
-                            </p>
-                        </div>
-                        <div class="testimonial-content">
-
-                            <div class="testimonial-author-info">
-                                <h4>Ninal Gordon</h4>
-                                <p>Parent</p>
-                            </div>
-                        </div>
-                        <span class="testimonial-quote-icon"><i class="far fa-quote-right"></i></span>
-                    </div>
                 </div>
             </div>
         </div>
         <!-- testimonial area end -->
+        @endif
 
 
         <!-- partner area -->
